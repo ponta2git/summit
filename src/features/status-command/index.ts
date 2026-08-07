90baf768968dc650a87605d85fb96a20ed37692f
ponta <coshun@gmail.com>
2026-08-07 20:39:36 +0900

    chore: remove unused source barrels

--- src/features/status-command/index.ts
+++
@@ -1,4 +0,0 @@
-export { statusCommandBuilder } from "./command.js";
-export { handleStatusCommand } from "./handler.js";
-export { buildStatusViewModel, renderStatusText } from "./viewModel.js";
-export { collectInvariantWarnings } from "./invariantChecks.js";
